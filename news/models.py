4395f5bef49523209d6a19295a0d549be76825b0
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2020-10-17 18:34:16 -0400

    updates

--- news/models.py
+++ news/models.py
@@ -4,8 +4,20 @@ from django.db import models
 
 
 class News(models.Model):
+
+    Coffee = "Coffee"
+    Music = "music"
+    Travel = "Travel"
+    STORY_CHOICES =[
+        (Coffee, "Coffee"),
+        (Music, "Music"),
+        (Travel, "Travel")
+    ]
     title = models.CharField(max_length=225)
     body = models.TextField()
+    category = models.CharField(max_length=225, choices=STORY_CHOICES, default=Music)
+    status = models.CharField(max_length=225)
+    image_url = models.URLField(max_length=225)
 
 
 
