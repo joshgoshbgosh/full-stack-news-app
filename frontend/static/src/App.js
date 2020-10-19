8fc475bc3bee67ad49ebdedd80011a721f968fb8
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2020-10-18 22:59:15 -0400

    update

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-
+import TravelList from './components/TravelList';
+import MusicList from './components/MusicList';
+import CoffeeList from './components/CoffeeList';
 import './App.css';
-import NewsForm from './NewsForm';
+import NewsForm from './components/NewsForm';
+import StoryList from './components/StoryList';
 
-function App() {
-  return(
-    <div><NewsForm /></div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      stories : [],
+    }
+
+
+
+componentDidMount() {
+  fetch('api/v1/')
+  .then(response => response.json())
+  .then(data => this.setState({stories: data}))
+  .catch(error => console.log('Error:', error));
+}
+
+
+
+
+  }
 }
 export default App;
